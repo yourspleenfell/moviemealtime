699d336ac7421a9a575cf7591f8d0315beda72c8
Cajayon <mcajayon@amazon.com>
2018-05-22 10:41:12 -0700

    add models

---
+++ src/main/java/com/darbuth/moviemealtime/MoviemealtimeApplication.java
@@ -0,0 +1,12 @@
+package com.darbuth.moviemealtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviemealtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviemealtimeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/darbuth/moviemealtime/MoviemealtimeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.darbuth.moviemealtime;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoviemealtimeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
